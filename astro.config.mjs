5d195c39a55511fbd9be3c9b3b259a6b2b1046bd
AnmarHani <AnmarHaniV1@gmail.com>
2025-05-17 12:33:21 +0300

    tTrying to include css instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   output: "static",
   outDir: "docs",
+  base: "/docs/",
   i18n: {
     locales: ["ar", "en"],
     defaultLocale: "en",
