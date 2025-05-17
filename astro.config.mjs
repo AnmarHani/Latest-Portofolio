ad664a15a709a2fc80c0181aa0b339a031772e60
AnmarHani <AnmarHaniV1@gmail.com>
2025-05-17 13:07:12 +0300

    Added some configurations and rebuild and added github ci/cd

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   output: "static",
   outDir: "docs",
+  site: "https://anmarhani.com",
   i18n: {
     locales: ["ar", "en"],
     defaultLocale: "en",
