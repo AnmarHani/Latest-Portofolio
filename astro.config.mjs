0493266c97298b45004fe2beee5cc2c2f2db5329
AnmarHani <AnmarHaniV1@gmail.com>
2025-05-18 09:38:45 +0300

    Testing without Docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  output: "static",
-  outDir: "docs",
   site: "https://anmarhani.com",
   i18n: {
     locales: ["ar", "en"],
